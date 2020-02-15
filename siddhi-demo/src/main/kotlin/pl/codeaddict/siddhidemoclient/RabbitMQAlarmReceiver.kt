4d724d997ca53148d0480d24c12eeda11df75f36
Michal Kostewicz <m.kostewicz84@gmail.com>
2020-02-15 12:24:10 +0100

    Proper alert queue configuration in application

--- siddhi-demo/src/main/kotlin/pl/codeaddict/siddhidemoclient/RabbitMQAlarmReceiver.kt
+++ siddhi-demo/src/main/kotlin/pl/codeaddict/siddhidemoclient/RabbitMQAlarmReceiver.kt
@@ -1,15 +1,20 @@
 package pl.codeaddict.siddhidemoclient
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler
-
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
 
 
-@RabbitListener(queues = ["alerts"])
+@Component
 class RabbitMQAlarmReceiver {
 
-    @RabbitHandler
-    fun receive(`in`: String) {
+    @RabbitListener(bindings = [QueueBinding(value = Queue(value = "alerts", durable = "true"),
+            exchange = Exchange(value = "direct_alerts", ignoreDeclarationExceptions = "true"),
+            key = ["alerts"])]
+    )
+    fun receiver(`in`: String) {
         println(" [x] Received '$`in`'")
     }
 }
