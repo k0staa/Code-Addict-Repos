516921a1e157075843c8435f5fb613adb02d54ee
Michal Kostewicz <m.kostewicz84@gmail.com>
2021-02-14 12:04:39 +0100

    Add CrossOrigin to controller

--- flutter-spring-kotlin-web/api/src/main/kotlin/pl/codeaddict/flutterapi/ApiController.kt
+++ flutter-spring-kotlin-web/api/src/main/kotlin/pl/codeaddict/flutterapi/ApiController.kt
@@ -1,6 +1,7 @@
 package pl.codeaddict.flutterapi
 
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -8,6 +9,7 @@ import reactor.core.publisher.Flux
 @RestController
 class ApiController {
 
+    @CrossOrigin
     @GetMapping(path = ["/numbers"],
         produces = [MediaType.APPLICATION_NDJSON_VALUE])
     fun getNumbers() = Flux.range(1, 100)
