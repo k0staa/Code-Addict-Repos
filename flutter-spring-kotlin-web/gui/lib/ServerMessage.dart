5bfa5d262038092bbcf483e321662582bdec398e
Michal Kostewicz <m.kostewicz84@gmail.com>
2021-02-14 12:21:24 +0100

    Add HTTP call to API and show message from it in application frame

---
+++ flutter-spring-kotlin-web/gui/lib/ServerMessage.dart
@@ -0,0 +1,10 @@
+
+class ServerMessage {
+  final String message;
+
+  ServerMessage({this.message});
+
+  factory ServerMessage.fromJson(Map<String, dynamic> json) {
+    return ServerMessage(message: json['message']);
+  }
+}
