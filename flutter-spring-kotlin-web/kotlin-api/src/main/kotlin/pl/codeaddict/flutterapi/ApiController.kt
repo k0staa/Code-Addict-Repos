02e2bb606bb9143b35dbba2819ae8d2592f1a339
Michal Kostewicz <m.kostewicz84@gmail.com>
2021-03-07 14:12:08 +0000

    Remove CrossOrigin adnotation and add cors filter to configurations

--- flutter-spring-kotlin-web/kotlin-api/src/main/kotlin/pl/codeaddict/flutterapi/ApiController.kt
+++ flutter-spring-kotlin-web/kotlin-api/src/main/kotlin/pl/codeaddict/flutterapi/ApiController.kt
@@ -1,7 +1,6 @@
 package pl.codeaddict.flutterapi
 
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -9,12 +8,10 @@ import reactor.core.publisher.Mono
 @RestController
 class ApiController {
 
-    @CrossOrigin
     @PreAuthorize("permitAll()")
     @GetMapping("/not-secured")
     fun getNonSecuredMessage() = Mono.just(ApiResponse("Server return non secured message"))
 
-    @CrossOrigin
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/secured")
     fun getSecuredMessage() = Mono.just(ApiResponse("Server return SECURED message"))
